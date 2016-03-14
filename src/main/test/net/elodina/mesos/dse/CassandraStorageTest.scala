7af94a15a0351305e4be8ee30a4ebf1bfebfe53c
Andrii Biletskyi <andrii.biletskyi@stealth.ly>
2016-03-14 11:59:32 +0200

    Issue-87 - nodes equality test

--- src/main/test/net/elodina/mesos/dse/CassandraStorageTest.scala
+++ src/main/test/net/elodina/mesos/dse/CassandraStorageTest.scala
@@ -2,7 +2,7 @@ package net.elodina.mesos.dse
 
 import java.util.Date
 
-import net.elodina.mesos.dse.Node.{Reservation, Failover, Stickiness}
+import net.elodina.mesos.dse.Node.{Failover, Reservation, Stickiness}
 import net.elodina.mesos.dse.Util.{BindAddress, Period}
 import org.cassandraunit.CassandraCQLUnit
 import org.cassandraunit.dataset.cql.FileCQLDataSet
@@ -10,7 +10,6 @@ import org.junit.Assert._
 import org.junit.{Before, Rule, Test}
 
 import scala.collection.JavaConverters._
-import scala.collection.mutable
 
 object CassandraStorageTest {
   //these should match cql dataset file
@@ -21,7 +20,7 @@ object CassandraStorageTest {
                             jmxRemote: Boolean, jmxUser: String, jmxPassword: String): Cluster = {
     val cluster = new Cluster(id)
     cluster.bindAddress = new BindAddress(bindAddress)
-    cluster.ports = new mutable.HashMap[Node.Port.Value, Util.Range]() ++= ports
+    cluster.ports = cluster.ports ++= ports
     cluster.jmxRemote = jmxRemote
     cluster.jmxUser = jmxUser
     cluster.jmxPassword = jmxPassword
@@ -58,15 +57,15 @@ object CassandraStorageTest {
     node.rack = rack
     node.dc = dc
 
-    node.constraints = new mutable.HashMap[String, List[Constraint]] ++= constraints
-    node.seedConstraints = new mutable.HashMap[String, List[Constraint]] ++= seedConstraints
+    node.constraints = node.constraints ++= constraints
+    node.seedConstraints = node.seedConstraints ++= seedConstraints
 
     node.dataFileDirs = dataFileDirs
     node.commitLogDir = commitLogDir
     node.savedCachesDir = savedCachesDir
 
-    node.cassandraDotYaml = new mutable.HashMap[String, String] ++= cassandraDotYaml
-    node.addressDotYaml = new mutable.HashMap[String, String] ++= addressDotYaml
+    node.cassandraDotYaml = node.cassandraDotYaml ++= cassandraDotYaml
+    node.addressDotYaml = node.addressDotYaml ++= addressDotYaml
     node.cassandraJvmOptions = cassandraJvmOptions
 
     node.modified = modified
@@ -76,37 +75,63 @@ object CassandraStorageTest {
     node
   }
 
-  private def clusterEquals(cluster: Cluster, other: Cluster): Boolean = {
-    cluster.id == other.id &&
-    cluster.bindAddress.values == other.bindAddress.values &&
-    cluster.ports == other.ports &&
-    cluster.jmxRemote == other.jmxRemote &&
-    cluster.jmxUser == other.jmxUser &&
-    cluster.jmxPassword == other.jmxPassword
+  private def assertClusterEquals(expected: Cluster, actual: Cluster): Unit = {
+    assertEquals(expected.id, actual.id)
+    assertEquals(expected.bindAddress.values, actual.bindAddress.values)
+    assertEquals(expected.ports, actual.ports)
+    assertEquals(expected.jmxRemote, actual.jmxRemote)
+    assertEquals(expected.jmxUser, actual.jmxUser)
+    assertEquals(expected.jmxPassword, actual.jmxPassword)
   }
 
-  private def nodeEquals(node:Node, other:Node): Boolean = {
-    node.id == other.id &&
-    node.state == other.state &&
-    node.cluster.id == other.cluster.id &&
-    node.stickiness == other.stickiness &&
-    node.runtime == other.runtime &&
-    node.cpu == other.cpu &&
-    node.mem == other.mem &&
-    node.seed == other.seed &&
-    node.jvmOptions == other.jvmOptions &&
-    node.rack == other.rack &&
-    node.dc == other.dc &&
-    node.constraints == other.constraints &&
-    node.seedConstraints == other.seedConstraints &&
-    node.dataFileDirs == other.dataFileDirs &&
-    node.commitLogDir == other.commitLogDir &
-    node.savedCachesDir == other.savedCachesDir &&
-    node.cassandraDotYaml == other.cassandraDotYaml &&
-    node.addressDotYaml == other.addressDotYaml &&
-    node.cassandraJvmOptions == other.cassandraJvmOptions &&
-    node.modified == other.modified &&
-    node.failover == other.failover
+  private def assertNodeEquals(expected: Node, actual: Node): Unit = {
+    assertEquals(expected.id, actual.id)
+    assertEquals(expected.state, actual.state)
+    assertEquals(expected.cluster.id, actual.cluster.id)
+
+    if (expected.stickiness == null || actual.stickiness == null) {
+      assertTrue(expected.stickiness == null && actual.stickiness == null)
+    } else {
+      assertEquals(expected.stickiness.period, actual.stickiness.period)
+      assertEquals(expected.stickiness.hostname, actual.stickiness.hostname)
+      assertEquals(expected.stickiness.stopTime, actual.stickiness.stopTime)
+    }
+
+    if (expected.runtime == null || actual.runtime == null) {
+      assertTrue(expected.runtime == null && actual.runtime == null)
+    } else {
+      assertEquals(expected.runtime.taskId, actual.runtime.taskId)
+      assertEquals(expected.runtime.executorId, actual.runtime.executorId)
+      assertEquals(expected.runtime.slaveId, actual.runtime.slaveId)
+      assertEquals(expected.runtime.hostname, actual.runtime.hostname)
+      assertEquals(expected.runtime.address, actual.runtime.address)
+      assertEquals(expected.runtime.seeds, actual.runtime.seeds)
+      assertEquals(expected.runtime.attributes, actual.runtime.attributes)
+      assertEquals(expected.runtime.reservation.cpus, actual.runtime.reservation.cpus, 0.0d)
+      assertEquals(expected.runtime.reservation.mem, actual.runtime.reservation.mem)
+      assertEquals(expected.runtime.reservation.ports, actual.runtime.reservation.ports)
+      assertEquals(expected.runtime.reservation.ignoredPorts, actual.runtime.reservation.ignoredPorts)
+    }
+
+    assertEquals(expected.cpu, actual.cpu, 0.0d)
+    assertEquals(expected.mem, actual.mem)
+    assertEquals(expected.seed, actual.seed)
+    assertEquals(expected.jvmOptions, actual.jvmOptions)
+    assertEquals(expected.rack, actual.rack)
+    assertEquals(expected.dc, actual.dc)
+    assertEquals(expected.constraints, actual.constraints)
+    assertEquals(expected.seedConstraints, actual.seedConstraints)
+    assertEquals(expected.dataFileDirs, actual.dataFileDirs)
+    assertEquals(expected.commitLogDir, actual.commitLogDir)
+    assertEquals(expected.savedCachesDir, actual.savedCachesDir)
+    assertEquals(expected.cassandraDotYaml, actual.cassandraDotYaml)
+    assertEquals(expected.addressDotYaml, actual.addressDotYaml)
+    assertEquals(expected.cassandraJvmOptions, actual.cassandraJvmOptions)
+    assertEquals(expected.modified, actual.modified)
+
+    assertEquals(expected.failover.delay, actual.failover.delay)
+    assertEquals(expected.failover.maxDelay, actual.failover.maxDelay)
+    assertEquals(expected.failover.maxTries, actual.failover.maxTries)
   }
 }
 
@@ -154,7 +179,7 @@ class CassandraStorageTest {
     val query =
       s"""
          |select ${CassandraStorage.NrOfNodes} from $CassandraTable
-         |where namespace='${Nodes.namespace}' and framework_id='${Nodes.frameworkId}' and cluster_id='${cluster.id}' and node_id='undefined'
+          |where namespace='${Nodes.namespace}' and framework_id='${Nodes.frameworkId}' and cluster_id='${cluster.id}' and node_id='undefined'
     """.stripMargin
     println(query)
     val cluster1Nodes = cassandraCQLUnit.session.execute(query).one().getInt(0)
@@ -173,7 +198,7 @@ class CassandraStorageTest {
 
     val node1_2 = {
       val reservation = new Reservation(1.0, 256, Map(Node.Port.CQL -> 5001), List(Node.Port.AGENT))
-      val runtime = new Node.Runtime("task_1_2", "executor_1_2", "slave0", "hostname.1.server", List("192.168.0.0"), reservation)
+      val runtime = new Node.Runtime("task_1_2", "executor_1_2", "slave0", "hostname.1.server", List("192.168.0.0"), reservation, Map("a1" -> "b1", "a2" -> "b2"))
       val failover = new Failover(new Period("100s"), new Period("50s"), 5)
       createNode("2", Node.State.RECONCILING, cluster2, createStickiness("1h", null, null), runtime, 0.5,
         1024, true, "opt1", "r1", "dc1", Map("hostname" -> List(Constraint("like:master*"))), Map("hostname" -> List(Constraint("like:slave*"))),
@@ -190,7 +215,7 @@ class CassandraStorageTest {
       createNode("3", Node.State.RECONCILING, cluster2, createStickiness("2h", null, null), runtime, 0.6,
         1024, true, "opt2", "r2", "dc2", Map("hostname" -> List(Constraint("like:slave*"))), Map("hostname" -> List(Constraint("like:master*"))),
         "/tmp/datafile2", "/tmp/comitlogdir2", "/opt/savedCaches2", Map("num_tokens" -> "2"), Map("stomp_interface" -> "10.3.2.1"), "copt2", false,
-      new Failover(new Period("20m"), new Period("10s"), null))
+        new Failover(new Period("20m"), new Period("10s"), null))
     }
     Nodes.addNode(node2_2)
 
@@ -202,7 +227,12 @@ class CassandraStorageTest {
 
     assertEquals("storage should load default and two added clusters", 3, Nodes.clusters.size)
     assertEquals(3, Nodes.nodes.size)
-    assertEquals(Set(cluster1, cluster2), Nodes.clusters.toSet)
-  }
+    assertEquals(Set(Nodes.defaultCluster, cluster1, cluster2), Nodes.clusters.toSet)
+    assertClusterEquals(cluster1, Nodes.getCluster(cluster1.id))
+    assertClusterEquals(cluster2, Nodes.getCluster(cluster2.id))
 
+    assertNodeEquals(node1_1, Nodes.getNode(node1_1.id))
+    assertNodeEquals(node1_2, Nodes.getNode(node1_2.id))
+    assertNodeEquals(node2_2, Nodes.getNode(node2_2.id))
+  }
 }
